b3886f037a04aad19121c7616e17a5c576cdb459
Gabriel Calasans <48914341+gabrielcalasans@users.noreply.github.com>
2023-08-09 04:24:23 -0300

    Add files via upload

--- bibliotecas.bat
+++ bibliotecas.bat
@@ -2,7 +2,7 @@ pip install yfinance==0.2.24
 pip install pandas==2.0.3
 pip install streamlit==1.24.1
 pip install streamlit-nested-layout==0.1.1
-pip install streamlit-tags=1.2.8
+pip install streamlit-tags==1.2.8
 pip install ta==0.10.2
 pip install mplfinance==0.12.9b7
 pip install sklearn==0.0.post5
